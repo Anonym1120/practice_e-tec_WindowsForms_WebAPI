c7b0d2988b9dcff5d2d94f3618c0c35a175c5ac3
Anonym <anonym1120@gmail.com>
2021-03-25 17:03:11 +0800

    GET、DELETE功能完成

---
+++ slnMyWindowsFormsWebAPI/prjMyWindowsFormsWebAPI/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace prjMyWindowsFormsWebAPI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboxMethods = new System.Windows.Forms.ComboBox();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.lblInput = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cboxMethods
+            // 
+            this.cboxMethods.BackColor = System.Drawing.SystemColors.Window;
+            this.cboxMethods.Font = new System.Drawing.Font("新細明體", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.cboxMethods.FormattingEnabled = true;
+            this.cboxMethods.Location = new System.Drawing.Point(26, 52);
+            this.cboxMethods.Name = "cboxMethods";
+            this.cboxMethods.Size = new System.Drawing.Size(171, 35);
+            this.cboxMethods.TabIndex = 0;
+            // 
+            // txtURL
+            // 
+            this.txtURL.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtURL.Location = new System.Drawing.Point(203, 51);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(351, 36);
+            this.txtURL.TabIndex = 1;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Font = new System.Drawing.Font("新細明體", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnSend.Location = new System.Drawing.Point(449, 93);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(105, 40);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtInput.Location = new System.Drawing.Point(26, 171);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(528, 207);
+            this.txtInput.TabIndex = 3;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtResult.Location = new System.Drawing.Point(26, 432);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(528, 207);
+            this.txtResult.TabIndex = 4;
+            // 
+            // lblInput
+            // 
+            this.lblInput.AutoSize = true;
+            this.lblInput.Font = new System.Drawing.Font("新細明體", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblInput.Location = new System.Drawing.Point(20, 136);
+            this.lblInput.Name = "lblInput";
+            this.lblInput.Size = new System.Drawing.Size(79, 32);
+            this.lblInput.TabIndex = 5;
+            this.lblInput.Text = "輸入";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("新細明體", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblResult.Location = new System.Drawing.Point(20, 397);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(79, 32);
+            this.lblResult.TabIndex = 6;
+            this.lblResult.Text = "結果";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.ClientSize = new System.Drawing.Size(584, 661);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.lblInput);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtURL);
+            this.Controls.Add(this.cboxMethods);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboxMethods;
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Label lblInput;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ slnMyWindowsFormsWebAPI/prjMyWindowsFormsWebAPI/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Net; //WebRequest
+using System.IO; //Stream
+using System.Web.Script.Serialization; //JavaScriptSerializer
+
+namespace prjMyWindowsFormsWebAPI
+{
+    public partial class Form1 : Form
+    {
+        List<string> methodsList = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            methodsList.Add("GET");
+            methodsList.Add("POST");
+            methodsList.Add("PUT");
+            methodsList.Add("DELETE");
+
+            foreach (string item in methodsList) 
+            {
+                cboxMethods.Items.Add(item);
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {            
+            WebRequest request = WebRequest.Create(txtURL.Text);
+            request.ContentType = "application/json";
+
+            if (cboxMethods.SelectedIndex == 0) //GET
+            {
+                request.Method = "GET";
+                request.ContentType = "application/json";
+                var response = request.GetResponse();
+                Stream stream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(stream);
+
+                var getResult = reader.ReadToEnd();
+                foreach (var item in getResult)
+                {
+                    txtResult.Text += item;
+                }
+
+                reader.Close();
+                stream.Close();
+                response.Close();
+            }
+            else if (cboxMethods.SelectedIndex == 1) //POST
+            { //待修正
+                request.Method = "POST";
+                request.ContentType = "application/json";
+
+                var streamWriter = new StreamWriter(request.GetRequestStream());
+                string json = new JavaScriptSerializer().Serialize(txtInput.Text);
+                streamWriter.Write(json);
+                streamWriter.Flush();
+                streamWriter.Close();
+
+                WebResponse response = request.GetResponse();
+                StreamReader reader = new StreamReader(response.GetResponseStream());
+                var result = reader.ReadToEnd();
+                foreach (var item in result)
+                {
+                    txtResult.Text += item;
+                }
+
+                response.Close();
+                reader.Close();
+            }
+            else if (cboxMethods.SelectedIndex == 2) //PUT
+            { //待修正
+                request.Method = "PUT";
+                request.ContentType = "application/json";
+
+                var stremWriter = new StreamWriter(request.GetRequestStream());
+                stremWriter.Write(txtInput.Text);
+                stremWriter.Flush();
+                stremWriter.Close();
+
+                WebResponse response = request.GetResponse();
+                StreamReader reader = new StreamReader(response.GetResponseStream());
+                txtResult.Text = reader.ReadToEnd();
+
+                response.Close();
+                reader.Close();
+            }
+            else if (cboxMethods.SelectedIndex == 3) 
+            {
+                request.Method = "DELETE";
+                request.ContentType = "application/json";
+
+                WebResponse response = request.GetResponse();
+                var reader = new StreamReader(response.GetResponseStream());
+
+                txtResult.Text = reader.ReadToEnd();
+
+                response.Close();
+                reader.Close();
+            }
+
+        }
+    }
+}
